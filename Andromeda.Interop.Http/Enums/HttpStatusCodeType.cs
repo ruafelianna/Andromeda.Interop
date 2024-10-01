be57c7793106b0fd8df3d0d36efc3da95114fb73
Ruafel <ruafelianna@ya.ru>
2024-10-01 11:33:09 +0300

    Add Http status code extensions

---
+++ Andromeda.Interop.Http/Enums/HttpStatusCodeType.cs
@@ -0,0 +1,12 @@
+namespace Andromeda.Interop.Http.Enums
+{
+    public enum HttpStatusCodeType
+    {
+        Unknown = 0,
+        Informational = 100,
+        Success = 200,
+        Redirection = 300,
+        ClientError = 400,
+        ServerError = 500,
+    }
+}
---
+++ Andromeda.Interop.Http/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,40 @@
+using Andromeda.Interop.Http.Enums;
+using System.Net.Http;
+
+namespace Andromeda.Interop.Http.Extensions
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static HttpStatusCodeType GetStatusType(
+            this HttpResponseMessage response
+        ) => (int)response.StatusCode switch
+        {
+            >= 100 and < 200 => HttpStatusCodeType.Informational,
+            >= 200 and < 300 => HttpStatusCodeType.Success,
+            >= 300 and < 400 => HttpStatusCodeType.Redirection,
+            >= 400 and < 500 => HttpStatusCodeType.ClientError,
+            >= 500 and < 600 => HttpStatusCodeType.ServerError,
+            _ => HttpStatusCodeType.Unknown,
+        };
+
+        public static bool IsInformational(
+            this HttpResponseMessage response
+        ) => response.GetStatusType() == HttpStatusCodeType.Informational;
+
+        public static bool IsSuccess(
+            this HttpResponseMessage response
+        ) => response.GetStatusType() == HttpStatusCodeType.Success;
+
+        public static bool IsRedirection(
+            this HttpResponseMessage response
+        ) => response.GetStatusType() == HttpStatusCodeType.Redirection;
+
+        public static bool IsClientError(
+            this HttpResponseMessage response
+        ) => response.GetStatusType() == HttpStatusCodeType.ClientError;
+
+        public static bool IsServerError(
+            this HttpResponseMessage response
+        ) => response.GetStatusType() == HttpStatusCodeType.ServerError;
+    }
+}
