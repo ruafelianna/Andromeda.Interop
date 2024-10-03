1779d11700d8a5154b2d4233283a14aa34e67b37
Ruafel <ruafelianna@ya.ru>
2024-10-03 08:43:05 +0300

    Add Modbus base and request read args implementation

---
+++ Andromeda.Interop.Modbus/Args/FC04_ReadInputRegisters/ArgsRequest_04.cs
@@ -0,0 +1,54 @@
+using Andromeda.Interop.Modbus.Abstractions.Args.FC04_ReadInputRegisters;
+using Andromeda.Interop.Modbus.Abstractions.Enums;
+
+namespace Andromeda.Interop.Modbus.Args.FC04_ReadInputRegisters
+{
+    public class ArgsRequest_04 :
+        ModbusArgsRequestReadBase,
+        IArgsRequest_04
+    {
+        public ArgsRequest_04(
+            ushort startAddress,
+            ushort cntRegs
+        ) : this(
+            IArgsRequest_04.StandardCode,
+            startAddress,
+            cntRegs
+        )
+        {
+        }
+
+        public ArgsRequest_04(
+            byte rawCode,
+            ushort startAddress,
+            ushort cntRegs
+        ) : base(
+            rawCode,
+            startAddress,
+            cntRegs
+        )
+        {
+        }
+
+        public ArgsRequest_04(
+            ModbusFunctionCodes enumCode,
+            ushort startAddress,
+            ushort cntRegs
+        ) : base(
+            enumCode,
+            startAddress,
+            cntRegs
+        )
+        {
+        }
+
+        public ushort QuantityOfInputRegisters
+            => QuantityOfItems;
+
+        protected override ushort MinQuantity
+            => IArgsRequest_04.MinQuantity;
+
+        protected override ushort MaxQuantity
+            => IArgsRequest_04.MaxQuantity;
+    }
+}
---
+++ Andromeda.Interop.Modbus/Args/ModbusArgsRequestReadBase.cs
@@ -0,0 +1,89 @@
+using Andromeda.Interop.Modbus.Abstractions.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Andromeda.Interop.Modbus.Args
+{
+    public abstract class ModbusArgsRequestReadBase : ModbusArgsBase
+    {
+        #region Ctor
+
+        public ModbusArgsRequestReadBase(
+            byte rawCode,
+            ushort startAddr,
+            ushort quantity
+        ) : base(rawCode)
+            => Init(
+                startAddr,
+                quantity,
+                out _startingAddress,
+                out _quantityOfItems
+            );
+
+        public ModbusArgsRequestReadBase(
+            ModbusFunctionCodes enumCode,
+            ushort startAddr,
+            ushort quantity
+        ) : base(enumCode)
+            => Init(
+                startAddr,
+                quantity,
+                out _startingAddress,
+                out _quantityOfItems
+            );
+
+        #endregion
+
+        #region Properties
+
+        public override IReadOnlyList<byte> RawData => [
+            unchecked((byte)(StartingAddress >> 8)),
+            unchecked((byte)(StartingAddress & 0xFF)),
+            unchecked((byte)(QuantityOfItems >> 8)),
+            unchecked((byte)(QuantityOfItems & 0xFF)),
+        ];
+
+        private readonly ushort _startingAddress;
+        public ushort StartingAddress => _startingAddress;
+
+        private readonly ushort _quantityOfItems;
+        protected ushort QuantityOfItems => _quantityOfItems;
+
+        protected abstract ushort MinQuantity { get; }
+
+        protected abstract ushort MaxQuantity { get; }
+
+        #endregion
+
+        #region Init
+
+        protected virtual void InitStartingAddress(
+            ushort address,
+            out ushort startingAddress
+        ) => startingAddress = address;
+
+        protected virtual void InitQuantityOfItems(
+            ushort quantity,
+            out ushort quantityOfItems
+        )
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(quantity, MinQuantity);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(quantity, MaxQuantity);
+
+            quantityOfItems = quantity;
+        }
+
+        private void Init(
+            ushort address,
+            ushort quantity,
+            out ushort startingAddress,
+            out ushort quantityOfItems
+        )
+        {
+            InitStartingAddress(address, out startingAddress);
+            InitQuantityOfItems(quantity, out quantityOfItems);
+        }
+
+        #endregion
+    }
+}
